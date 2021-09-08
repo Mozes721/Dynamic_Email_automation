7fa4c7f049c01d7d893ab4f559641b7101601f2a
Mozes721 <Richard.Taujenis@gmail.com>
2021-09-08 16:47:57 +0200

    prompt options

--- main.py
+++ main.py
@@ -3,17 +3,44 @@ import smtplib
 from email.message import EmailMessage
 import phonenumbers as phonenumbers
 from phonenumbers import geocoder
- 
-df = pd.read_csv('contacts.csv')
 
-print(df.columns)
+#read csv file
+df = pd.read_csv('contacts.csv')
 
 #get email
 print(df['email'])
 
 my_contact = df.loc[(df['first_name'] == 'Richard') & (df['last_name'] == 'Taujenis'), ['email']]
 print(my_contact)
-	
+
+
+ #if individiual
+def individual():
+   print("individual pressed")
+      #if bulk
+def bulk():
+   print("bulk pressed")
+      
+#prompt if send email to all contact or individual
+def main():
+   running = True
+   prompt = int(input('''Do you want to send to individual contact email or all:\n For individual press 1 \n For bulk send press 2 \n>'''))
+   while running:
+      try:
+         if prompt == 1:
+            individual()
+            running = False
+         if prompt == 2:
+            bulk()
+            running = False
+         else:
+            print("Try again")
+         break
+      except ValueError as e:
+         print("wrong value given")
+         break
+         
+     
 
 if __name__ == '__main__':
-   pass
+   main()
