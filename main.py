d055b2f94a43554a7e1eb7d8b21aa37f79c2cfce
Mozes721 <Richard.Taujenis@gmail.com>
2021-09-10 18:17:21 +0200

    changed contacts and main

--- main.py
+++ main.py
@@ -16,31 +16,38 @@ print(my_contact)
  
  #if individiual
 def individual():
-   print("individual pressed")
+   print("Individual chosen")
+   print('#'*10)
+   for user in df['first_name'] + ' ' + df['last_name']:
+      print(user)
+   print('#'*10)
+   try:
+      user = input('Please write the user you want to send the email to: ')
+      if user in df['first_name'] + ' ' + df['last_name']:
+         print("Chosen " + user)
+      else:
+         print("User not in list")
+   except ValueError:
+      print("Wrong value given!")
       #if bulk
 def bulk():
    print("bulk pressed")
       
 #prompt if send email to all contact or individual
 def main():
-   running = True
-   prompt = int(input('''Do you want to send to individual contact email or all:\n For individual press 1 \n For bulk send press 2 \n>'''))
-   while running:
-      try:
-         if prompt == 1:
-            individual()
-            running = False
-         if prompt == 2:
-            bulk()
-            running = False
-         else:
-            print("Try again")
-         break
-      except ValueError as e:
-         print("wrong value given")
-         break
+   while True:
+   try:
+      prompt = int(input('''Do you want to send to individual contact email or all:\n For individual press 1 \n For bulk send press 2 \n>'''))
+      if prompt == 1:
+         individual()
+      if prompt == 2:
+         bulk()
+      else:
+         print("Only 1 or 2 is accepted. Try again...")
+   except Exception:
+      print("Sorry. Not accepted comand. Try again...")
+      
          
-     
-
+   
 if __name__ == '__main__':
    main()
