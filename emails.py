76ac38eaab29e56c9036fe89a7a251b9c9c0a07c
Mozes721 <Richard.Taujenis@gmail.com>
2021-09-18 00:26:24 +0200

    emails

---
+++ emails.py
@@ -0,0 +1,16 @@
+f = open('email_text.txt')
+
+email_text = f.read()
+content_split = email_text.splitlines()
+
+meetup_subject = content_split[0]
+meetup_text = content_split[1]
+
+job_rejection_subject =  content_split[2]
+job_rejection_text =  content_split[3]
+
+discount_subject = content_split[4]
+discount_text = content_split[5]
+
+subjects = [meetup_subject, job_rejection_subject, discount_subject]
+texts = [meetup_text, job_rejection_text, discount_text]
